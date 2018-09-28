87df3dd8f1087c200a39550a54bf28c846eaa63a
potapovDim <potapovd3012@gmail.com>
2018-09-28 19:40:40 +0300

    add click

--- x.js
+++ x.js
@@ -14,3 +14,46 @@ console.log('sync func 7')
 
 setTimeout(() => console.log('setTimeout'), 0)
 setImmediate(() => console.log('setImmediate'))
+
+
+function clickOn(el) {
+  const {bottom, left, right, top} = el.getBoundingClientRect()
+  const mouseClick = document.createEvent('MouseEvent')
+  const mouseMouseUp = document.createEvent('MouseEvent')
+
+  const elYCenter = top + ((bottom - top) / 2) // as a clientY
+  const elXCenter = left + ((right - left) / 2) // as a clientX
+
+  const screenX = window.screenX + elXCenter // as a screenX
+  const screenY = window.screenY + elYCenter // as a screenX
+
+  mouseClick.initMouseEvent(
+    'mousedown', true, false, window, 1, // detail : Event's mouse click count
+    screenX, // screenX
+    screenY, // screenY
+    elXCenter, // clientX
+    elYCenter, // clientY
+    false, // ctrlKey
+    false, // altKey
+    false, // shiftKey
+    false, // metaKey
+    0, // button : 0 = click, 1 = middle button, 2 = right button
+    null // relatedTarget : Only used with some event types (e.g. mouseover and mouseout). In other cases, pass null.
+  )
+
+  mouseMouseUp.initMouseEvent(
+    'mouseup', true, false, window, 1, // detail : Event's mouse click count
+    screenX, // screenX
+    screenY, // screenY
+    elXCenter, // clientX
+    elYCenter, // clientY
+    false, // ctrlKey
+    false, // altKey
+    false, // shiftKey
+    false, // metaKey
+    0, // button : 0 = click, 1 = middle button, 2 = right button
+    null // relatedTarget : Only used with some event types (e.g. mouseover and mouseout). In other cases, pass null.
+  )
+  el.dispatchEvent(mouseClick)
+  el.dispatchEvent(mouseMouseUp)
+}

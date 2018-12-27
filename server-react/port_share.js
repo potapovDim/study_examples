515b5d1abf8f2cf3491f0e7027a5b606b7b250d5
potapovDim <potapovd3012@gmail.com>
2018-12-27 08:16:55 +0200

    updates

--- server-react/port_share.js
+++ server-react/port_share.js
@@ -4,7 +4,7 @@ const path = require('path')
 const random_port = (length = 4) => {
   const num_str = '01234567890'
   let base_port = '8'
-  for(let i = 0; i < length; i++) {
+  for(let i = 0; i < length - 1; i++) {
     base_port += num_str[i]
   }
   return base_port
@@ -13,8 +13,17 @@ const random_port = (length = 4) => {
 const set_random_port = () => {
   const port_share_path = './port_share.json'
   const port = Number(random_port)
+  const write_port_obj = () => {
+    const PORT = random_port()
+    const port_obj = {PORT}
+    fs.writeFileSync(path.resolve(process.cwd(), port_share_path), JSON.stringify(port_obj))
+    return PORT
+  }
   if(fs.existsSync(path.resolve(process.cwd(), port_share_path))) {
-
+    fs.unlink(port_share_path)
+    return write_port_obj()
+  } else {
+    return write_port_obj()
   }
 }
 
--- server-react/simple_service1.js
+++ server-react/simple_service1.js
@@ -3,6 +3,11 @@ const bodyParser = require('koa-bodyparser')
 const cors = require('koa-cors')
 const fs = require('fs')
 const path = require('path')
+const {set_random_port} = require('./port_share')
+
+
+const second_service_port = set_random_port()
+const {fetchy_util: connection_second_service} = require('./request_util')(`http://localhost:${second_service_port}`)
 
 function save_JSON_data(data) {
   const data_JSON_path = './temp/data.json'
@@ -30,13 +35,11 @@ function dataToJSON(data) {
 
 // service works with JSON format files
 
-
-const {fetchy_util} = require('./request_util')('http://localhost:8080')
-
 const servise_1_action_types = {
   ASSERT_CONNECTION: 'ASSERT_CONNECTION',
   ASSERT_CONNECTION_ENVIRONMENT: 'ASSERT_CONNECTION_ENVIRONMENT',
-  SAVE_JSON_DATA: 'SAVE_JSON_DATA'
+  SAVE_JSON_DATA: 'SAVE_JSON_DATA',
+  GET_JSON_DATA: 'GET_JSON_DATA'
 }
 
 const app = new Koa()
@@ -44,12 +47,12 @@ app.use(cors())
 app.use(bodyParser())
 
 const request_worker = async (cntx) => {
-  const {request: {body: {action}}} = cntx
+  const {request: {body: {action, token, username}}} = cntx
   switch(action) {
     case servise_1_action_types.ASSERT_CONNECTION:
       return cntx.body = {connection: 'ok'}
     case servise_1_action_types.ASSERT_CONNECTION_ENVIRONMENT: {
-      const {body} = await fetchy_util.post('/', {action: 'ASSERT_CONNECTION'})
+      const {body} = await connection_second_service.post('/', {action: 'ASSERT_CONNECTION'})
       if(body.connection) {
         return cntx.body = {service_connection: 'ok'}
       }
@@ -59,8 +62,9 @@ const request_worker = async (cntx) => {
       return cntx.body = {ok: 'ok'}
   }
 }
-
-
+// set random port for service_
+set_random_port()
+const {fetchy_util} = require('./request_util')('http://localhost:8080')
 app.use(request_worker)
 
 app.listen(8081)

3fb2a2f92681f6649870d1ac3f332021436c483d
potapovDim <potapovd3012@gmail.com>
2018-12-28 10:11:25 +0200

    refactor

--- server-react/__specs__/simple.spec.js
+++ server-react/__specs__/simple.spec.js
@@ -3,6 +3,7 @@ const {expect} = require('chai')
 
 describe('Simple test execution', () => {
   let token = ''
+  const data = [{a: 'c'}]
   it('get token', async () => {
     const body_data = {action: 'AUTORIZATION'}
     const {body, status} = await fetchy.post('/', body_data)
@@ -12,7 +13,16 @@ describe('Simple test execution', () => {
     expect(body).haveOwnProperty('token')
   })
   it('save new data ', async () => {
-    const body_data = {action: 'SAVE_JSON_DATA', token, data: [{a: 'c'}]}
+    const body_data = {action: 'SAVE_JSON_DATA', token, data}
+    const {body, status} = await fetchy.post('/', body_data)
+    // console.log(status)
+    // console.log(body)
+    expect(status).to.eql(200)
+    expect(body).haveOwnProperty('json_data')
+    expect(body.json_data).to.eql('ok')
+  })
+  it('get json data ', async () => {
+    const body_data = {action: 'GET_JSON_DATA', token}
     const {body, status} = await fetchy.post('/', body_data)
     console.log(status)
     console.log(body)

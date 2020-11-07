0a48502a1c4ac2bd3dc268f697887e679d777340
Sniper <Sniper>
2020-11-07 21:19:15 +0300

    add login test + small orthographic fix

--- api-testing/specs/user.spec.ts
+++ api-testing/specs/user.spec.ts
@@ -4,7 +4,12 @@ import {serviceProvider} from '../framework'
 describe('User', function() {
   it('user-data', async () => {
     const {body, status} = await serviceProvider.user.getUserData()
-    expect(status).to.equal(200);
+    expect(status).to.equal(200)
     expect(body).to.include.keys('username', 'postal_code')
+  })
+  it('user-login', async () => {
+    const {body, status} = await serviceProvider.user.userLogin( {username: 'test_user', password: 'test_pass'})
+    expect(status).to.equal(200)
+    expect(body).to.include.keys('authorization')
   })
 })
